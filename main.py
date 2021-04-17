430273d5ce7f8289053b5af478f4f87e8971170b
Zuzanna Filipkowska <zuzanna.filipkowska.stud@pw.edu.pl>
2021-04-17 23:39:56 +0200

    Task 2 from homework

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 app = FastAPI()
+
+
+class MethodResp(BaseModel):
+    msg: str
 
 
 @app.get("/")
 def root():
     return {"message": "Hello world!"}
+
+
+@app.get("/method/{type}", response_model=MethodResp)
+def method_type(type: str):
+    return MethodResp(msg=f'{type}'.upper())
