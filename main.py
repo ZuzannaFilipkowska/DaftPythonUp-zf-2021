b1e0af247d5ef7c5e2e5df99ffbda03bf341c5f3
Zuzanna Filipkowska <zuzanna.filipkowska.stud@pw.edu.pl>
2021-04-20 13:29:45 +0200

    Zad 1.4

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI, Response
 from pydantic import BaseModel
 import hashlib
+import datetime
+from datetime import timedelta
 
 
 app = FastAPI()
@@ -10,9 +12,13 @@ class MethodResp(BaseModel):
     msg: str
 
 
+# 1.1
+
 @app.get("/")
 def root():
     return {"message": "Hello world!"}
+
+# 1.2
 
 
 @app.post("/method", status_code=201)
@@ -44,5 +50,38 @@ def get_method_delete():
 
 @app.get("/auth",  status_code=204)
 def password_auth(password=None, password_hash=None):
-    if not password_hash or password == "" or password_hash == "" or str(password_hash) != hashlib.sha512(str(password).encode("utf-8")).hexdigest()  or not password:
+    if not password_hash or password == "" or password_hash == "" or str(password_hash) != hashlib.sha512(str(password).encode("utf-8")).hexdigest() or not password:
         return Response(status_code=401)
+
+
+# 1.4
+
+app.id = 0
+
+
+class Patient(BaseModel):
+    name: str
+    surname: str
+
+
+class RegisteredPatient(BaseModel):
+    id: int
+    name: str
+    surname: str
+    register_date: str
+    vaccination_date: str
+
+
+@app.post('/register', response_model=RegisteredPatient, status_code=201)
+def register(patient: Patient):
+    app.id += 1
+    app.dateTime = datetime.date.today()
+    delta = timedelta(days=len(patient.name + patient.surname))
+    vacc_date = app.dateTime + delta
+    reg_patient = {
+            "id": app.id,
+            "name": patient.name,
+            "surname": patient.surname,
+            "register_date": str(app.dateTime),
+            "vaccination_date": str(vacc_date)}
+    return reg_patient
