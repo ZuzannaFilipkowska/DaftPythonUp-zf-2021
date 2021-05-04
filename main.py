d402a975fba25e27663a6112f09b43a2af525309
Zuzanna Filipkowska <zuzanna.filipkowska.stud@pw.edu.pl>
2021-05-04 22:09:34 +0200

    3.2

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI, Response, status, Request, HTTPException
 from pydantic import BaseModel
 import hashlib
+import random
+import string
 from datetime import datetime, timedelta
 from typing import Dict, Optional, List
 from fastapi import Query
@@ -25,4 +27,20 @@ def index_static(request: Request):
 
 # 3.2
 
+@app.post("/login_session", status_code=201)
+def login(user: str, password: str, response: Response):
+    if user != "4dm1n" or password != "NotSoSecurePa$$":
+        raise HTTPException(status_code=401)
+    random_string = "".join(random.choice(string.ascii_letters) for i in range(20))
+    token = random_string
+    response.set_cookie(key="session_token", value=token)
 
+
+@app.post("/login_token", status_code=201)
+def get_token(user: str, password: str, response: Response):
+    if user != "4dm1n" or password != "NotSoSecurePa$$":
+        raise HTTPException(status_code=401)
+    random_string = "".join(random.choice(string.ascii_letters) for i in range(20))
+    token = random_string
+    response.set_cookie(key="session_token", value=token)
+    return {"token": token}
